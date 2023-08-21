91dd1aaa11c7b0cd9755a250c4288dfca0ccc07a
Preet1202 <preet.patel@marutitech.com>
2023-08-21 08:09:41 +0530

    first commit

---
+++ BBMS/Repository/IBloodGroupRepository.cs
@@ -0,0 +1,24 @@
+﻿using BBMS.Data;
+using BBMS.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BBMS.Repository
+{
+    public interface IBloodGroupRepository
+    {
+        Task<int> AddPetientRequest(PatientRequestModel model);
+        Task<List<BloodGroupModel>> GetAllBloodGroup();
+        Task<List<PatientRequestModel>> GetPetientRequestDetails();
+        List<object> GetRequestStatusCount();
+        Task<int> AddDonateRequest(DonateRequestModel model);
+        Task<List<DonateRequestModel>> GetDonateRequestDetails();
+        Task<List<DonateRequestModel>> GetDonationDetailsAdmin();
+        Task<List<PatientRequestModel>> GetPetientRequestsAdmin();
+        bool UpdateRequestStatusForPetient(int id, string action);
+        bool UpdateRequestStatusForDonor(int id, string action);
+        bool UpdateBloodStock(BloodGroupModel model);
+
+
+    }
+}
